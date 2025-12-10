3a94d7710cecf8fb41ba9a0550ad3e7f4f516acd
Nastya E <dplu99ed@gmail.com>
2025-12-10 19:23:06 +0500

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/GitHub-Profile/",
+  build: { outDir: "docs", emptyOutDir: true },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
